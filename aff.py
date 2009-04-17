7196ce2c7d659af6145bb161357da1edf9421edf
Namhyung Kim <namhyung@gmail.com>
2009-04-17 12:17:24 +0900

    이메일 주소 수정 (% => @)

--- aff.py
+++ aff.py
@@ -22,7 +22,7 @@
 # the Initial Developer. All Rights Reserved.
 #
 # Contributor(s): Changwoo Ryu <cwryu@debian.org>
-# Namhyung Kim <namhyung%gmail.com>
+# Namhyung Kim <namhyung@gmail.com>
 #
 # Alternatively, the contents of this file may be used under the terms of
 # either the GNU General Public License Version 2 or later (the "GPL"), or
