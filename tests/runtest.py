e76ecc376722aa57f2083e5604311c0ff0ad2fc8
Changwoo Ryu <cwryu@debian.org>
2009-08-23 12:53:21 +0900

    두벌식 오타 추천 규칙 추가
    - ㅇ벗어 => 없어, 젭라 => 제발
    - 테스트 케이스 추가

--- tests/runtest.py
+++ tests/runtest.py
@@ -29,17 +29,33 @@ def main():
             continue
         if l[0] == '#':         # comment
             continue
-        (flag, word) = l.split(' ')
+        tokens = l.split(' ')
+        flag = tokens[0]
+        word = tokens[1]
+        args = tokens[2:]
+
+        hunspell.stdin.write('%s\n' % word)
+        result = hunspell.stdout.readline().strip()
+        hunspell.stdout.readline() # empty line
         if flag == 'Y' or flag == 'N':
-            hunspell.stdin.write('%s\n' % word)
-            result = hunspell.stdout.readline()
-            hunspell.stdout.readline() # empty line
             if ((flag == 'Y' and 
                  result[0] != '*' and result[0] != '+' and result[0] != '-') or
                 (flag == 'N' and
                  result[0] != '&' and result[0] != '#')):
                     errmsg('%s:%d: %s %s: %s\n' % (filename, lineno, flag,
-                                                   word, result[:-1]))
+                                                   word, result))
+                    sys.exit(1)
+        elif flag == 'S':
+            if result[0] != '&':
+                errmsg('%s:%d: %s %s: %s\n' % (filename, lineno, flag,
+                                               word, result))
+                sys.exit(1)
+            else:
+                suggests = result[result.find(':')+2:].split(', ')
+                if suggests[0] != args[0]:
+                    errmsg('%s:%d: %s %s %s: %s\n' % (filename, lineno, flag,
+                                                      word, args[0],
+                                                      result))
                     sys.exit(1)
         else:
             errmsg('%s:%d: invalid format\n')
