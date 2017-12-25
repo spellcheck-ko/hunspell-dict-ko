e5d35b23e72e9c41ea313b07772ebff64e9eb8c2
Changwoo Ryu <cwryu@debian.org>
2017-12-25 16:30:13 +0900

    0.7.1 release

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-version = '0.7.0-dev'
+version = '0.7.1'
 
 # 필요한 최소 hunspell 버전.
 # - 코드에서는 if config.required_hunspell_version >= (1,2,14): 와 같이
@@ -14,12 +14,12 @@ if required_hunspell_version < (1, 3, 1):  # 1.2.x에서는 합성어 관련해 
 
 # 내부 인코딩
 # - 'NFD': 유니코드 NFD 형태, U+1100에 있는 한글 자모
-# - '2+RST': 두벌식 + 리셋 코드
+# - '2+RST': 두벌식 + 리셋 코드 (개발 중)
 internal_encoding = 'NFD'
 # internal_encoding = '2+RST'
 
-# 어근 정보 포함
+# 어근 정보 포함 (개발 중)
 output_word_stem = False
 
-# 형태소 분석 정보 포함
+# 형태소 분석 정보 포함 (개발 중)
 output_word_morph = False
