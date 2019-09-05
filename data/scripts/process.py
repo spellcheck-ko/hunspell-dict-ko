06469104dc6c40f530b96a35e99495100b1d19df
Changwoo Ryu <cwryu@debian.org>
2019-09-05 20:53:20 +0900

    한국어기초사전 조사 문법 정보 추출  (진행 중)
    - "~ 뒤에 붙여 쓴다"가 있는 경우만 찾아낸다.
    
    https://github.com/spellcheck-ko/hunspell-dict-ko/issues/41

--- data/scripts/process.py
+++ data/scripts/process.py
@@ -62,7 +62,7 @@ class ProcessYamlDocs:
     def process_doc_krdict(self, input, output):
         if input['의미'][0]['뜻풀이'].startswith('→'):
             output['제외'] = '틀린 말'
-        elif input['품사'] in ['어미', '접사', '조사']:
+        elif input['품사'] in ['어미', '접사']:
             output['제외'] = '해당 품사 아님'
         else:
             word = input['표제어']
@@ -94,6 +94,30 @@ class ProcessYamlDocs:
             if pos in ['명사', '의존 명사']:
                 if '주제 및 상황 범주' in input and input['주제 및 상황 범주'] == '개념 > 세는 말':
                     props.append('단위명사')
+
+            # 조사 문법
+            if pos == '조사':
+                clues = []
+                if '참고' in input:
+                    for sentence in input['참고'].split('.'):
+                        sentence = sentence.strip()
+                        if len(sentence) > 0:
+                            clues.append(sentence)
+                for m in input['의미']:
+                    if '참고' in m:
+                        for sentence in m['참고'].split('.'):
+                            sentence = sentence.strip()
+                            if len(sentence) > 0:
+                                clues.append(sentence)
+                if len(clues) == 0:
+                    # 문법이 없으면 일반적인 체언 뒤에 붙는 것으로 가정한다
+                    output['조사 조합'] = ['체언']
+                else:
+                    josa_combinations = self.detect_josa_combination_krdict(clues)
+                    if len(josa_combinations) > 0:
+                        output['조사 조합'] = josa_combinations
+                    else:
+                        output['제외'] = '조사 문법 정보 부족'
 
             if len(props) > 0:
                 props.sort()
@@ -372,6 +396,108 @@ class ProcessYamlDocs:
             if prefix_nfd[-1] in [V_A, V_AE, V_E, V_EO]:
                 return True
         return False
+
+    def detect_josa_combination_krdict(self, clues):
+        prefix_list = []
+        if len(clues) == 0:
+            # 아무 정보가 없으면 모든 체언 뒤에 붙는 걸로 취급
+            prefix_list.append('체언')
+            return prefix_list
+
+        for clue in clues:
+            used = False
+
+            if clue.startswith('주로 '):
+                continue
+
+            if '나타내는 ' in clue:
+                s = clue.split('나타내는 ')[1]
+            elif '나타내는, ' in clue:
+                s = clue.split('나타내는, ')[1]
+            else:
+                s = clue
+
+            if s.startswith('‘ㄹ’을 제외한 받침 있는 '):
+                prefix_list.append('ㄹ 제외한 받침 있음')
+                s = s[len('‘ㄹ’을 제외한 받침 있는 '):]
+            elif s.startswith('받침이 없거나 ‘ㄹ’ 받침으로 끝나는 '):
+                prefix_list.append('받침 없거나 ㄹ 받침')
+                s = s[len('받침이 없거나 ‘ㄹ’ 받침으로 끝나는 '):]
+            elif s.startswith('받침이 없거나 ‘ㄹ’ 받침인 '):
+                prefix_list.append('받침 없거나 ㄹ 받침')
+                s = s[len('받침이 없거나 ‘ㄹ’ 받침인 '):]
+            elif s.startswith('받침이 없거나 ‘ㄹ’, ‘ㅆ’, ‘ㅄ’ 받침인 '):
+                prefix_list.append('받침 없거나 ㄹㅆㅄ 받침')
+                s = s[len('받침이 없거나 ‘ㄹ’, ‘ㅆ’, ‘ㅄ’ 받침인 '):]
+            elif s.startswith('받침 없는 '):
+                prefix_list.append('받침 없음')
+                s = s[len('받침 없는 '):]
+            elif s.startswith('받침 있는 '):
+                prefix_list.append('받침 있음')
+                s = s[len('받침 있는 '):]
+
+            prefix_found = False
+            if '뒤에 붙여 ' in s:
+                s = s.split(' 뒤에 붙여 ')[0]
+                prefix_found = True
+            elif  '뒤에 붙어 ' in s:
+                s = s.split(' 뒤에 붙어 ')[0]
+                prefix_found = True
+            elif '에 붙여 쓴다' in s:
+                s = s.split('에 붙여 쓴다')[0]
+                prefix_found = True
+
+            if prefix_found:
+                if s == '말':
+                    prefix_list.append('체언')
+                    used = True
+                elif '명사' in s:
+                    prefix_list.append('명사')
+                    used = True
+                elif '체언' in s:
+                    prefix_list.append('체언')
+                    used = True
+                elif '부사어' in s:
+                    prefix_list.append('부사어')
+                    used = True
+                elif '조사' in s:
+                    prefix_list.append('조사')
+                    used = True
+            elif s.endswith(' 온다'):
+                continue
+            elif s.endswith(' 쓸 수 있다'):
+                continue
+            elif s.endswith('로 쓴다'):
+                continue
+            elif s.endswith('활용을 한다'):
+                continue
+            elif s.endswith('잘 쓰지 않는다'):
+                continue
+            elif s.endswith('함께 쓴다'):
+                continue
+
+            if not used:
+                print('사용되지 않은 조사 참고: %s (%s)' % (clue,s))
+
+        found = False
+        for prefix in prefix_list:
+            if prefix in ['명사', '대명사', '수사']:
+                found = True
+
+        if not found:
+            prefix_list = []
+        else:
+            # remove duplicates
+            new_prefix_list = []
+            for prefix in prefix_list:
+                # 형태가 다를 수는 있어도 명사만 받는 조사는 없다
+                if prefix in ['명사']:
+                    if '체언' not in new_prefix_list:
+                        new_prefix_list.append('체언')
+            prefix_list = new_prefix_list
+            prefix_list.sort()
+
+        return prefix_list
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
